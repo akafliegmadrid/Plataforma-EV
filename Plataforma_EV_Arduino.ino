a584aa268bf9ed7aaa5170be4fe4db7253ed1501
PilotoF-22 <44830795+PilotoF-22@users.noreply.github.com>
2018-11-07 09:06:31 +0100

    Update Plataforma_EV_Arduino.ino

--- Plataforma_EV_Arduino.ino
+++ Plataforma_EV_Arduino.ino
@@ -10,20 +10,20 @@ SoftwareSerial mySerial(2,3);
 
 void setup(){
 				
-				mySerial.begin(9600);
-				Serial.begin(9600);
-				delay(100);
+	mySerial.begin(9600);
+	Serial.begin(9600);
+	delay(100);
 							
 }
 
 void loop(){
 				
-				if (Serial.available()>0){
-								mySerial.write(Serial.read());
-				}
+	if (Serial.available()>0){
+		mySerial.write(Serial.read());
+	}
 				
-				if (mySerial.available()>0){
-								Serial.write(mySerial.read());
-				}
+	if (mySerial.available()>0){
+		Serial.write(mySerial.read());
+	}
 				
 }
