2c50505465a1965603c0446958a7f035640f925b
PilotoF-22 <44830795+PilotoF-22@users.noreply.github.com>
2018-11-07 09:04:31 +0100

    Add files via upload

---
+++ Plataforma_EV_Arduino.ino
@@ -0,0 +1,29 @@
+// *****************************************
+// ***     PLATAFORMA EV ARDUINO         ***
+// ***        AKAFLIEG MADRID            ***
+// *** UNIVERSIDAD POLITECNICA DE MADRID ***
+// *****************************************
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2,3);
+
+void setup(){
+				
+				mySerial.begin(9600);
+				Serial.begin(9600);
+				delay(100);
+							
+}
+
+void loop(){
+				
+				if (Serial.available()>0){
+								mySerial.write(Serial.read());
+				}
+				
+				if (mySerial.available()>0){
+								Serial.write(mySerial.read());
+				}
+				
+}
