8c5e54e6669c167f1972cfaae7df634152a2cca7
PilotoF-22 <44830795+PilotoF-22@users.noreply.github.com>
2018-11-29 00:24:08 +0100

    Código final depurado

--- Plataforma_EV_Arduino.ino
+++ Plataforma_EV_Arduino.ino
@@ -4,73 +4,112 @@
 // *** UNIVERSIDAD POLITECNICA DE MADRID ***
 // *****************************************
 
-#include <SoftwareSerial.h>
-#include <SimpleDHT.h>
+//Librerías usadas
+  
+  #include <SoftwareSerial.h>
+  #include <dht_nonblocking.h>
 
 //Módulo Bluetooh
-SoftwareSerial mySerial(2,3); //Pines (Tx,Rx). Ojo conectar a 3.3V no 5V
+  
+  SoftwareSerial mySerial(2,3); //Pines (Tx,Rx) del BLE. Ojo conectar a 3.3V no 5V
 
 //Termistor
-int tempPin = 0; // Pin analógico
+  
+  int tempPin = A0; // Pin data (analógico)
 
 //Sensor temperatura y humedad
-int pinDHT11 = 2; // Pin digital
-SimpleDHT11 dht11;
+  
+  #define DHT_SENSOR_TYPE DHT_TYPE_11
+  static const int DHT_SENSOR_PIN = 4; //Pin data (digital)
+  DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+//Interruptor principal
+  
+  int system_recording = 0; // 0: No emite; 1: Emite
+  int switch_recording = 5; // Pin 5
 				
 void setup(){
-				
-  mySerial.begin(9600);
-  Serial.begin(9600);
-  delay(100);
-							
+	
+	//Inicializar monitores serial			
+    
+    mySerial.begin(9600); // Serial del BLE
+    Serial.begin(9600); // Serial del monitor PC
+  
+  //Interruptor principial
+    
+    pinMode(switch_recording,INPUT_PULLUP);  
+
+  delay(100);       
 }
 
 void loop(){
- //Modulo Bluetooh
-	 if (Serial.available()>0){
-		mySerial.write(Serial.read());
-	 }
+
+  //Interruptor principal
+  
+  if( (digitalRead(switch_recording) == LOW)){system_recording = 1;}
+  else {system_recording = 0;}
+
+  if( system_recording == 1){
+  
+  //Modulo Bluetooh
+  
+    if (Serial.available()>0){
+		  mySerial.write(Serial.read());
+	  }
 				
-	 if (mySerial.available()>0){
-	   	Serial.write(mySerial.read());
-	 }
+	  if (mySerial.available()>0){
+	    Serial.write(mySerial.read());
+	  }
  
- //Termistor
+  //Termistor
+  
+	  int tempReading = analogRead(tempPin);
+    double tempK_term = log(10000.0 * ((1024.0 / tempReading - 1)));
+		  tempK_term = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK_term * tempK_term )) * tempK_term );       //  Temp Kelvin
+    float tempC_term = tempK_term - 273.15;            // Convert Kelvin to Celcius
 				
-	int tempReading = analogRead(tempPin);
+  //Módulo temperatura y humedad
+	  
+    float tempC_DHT;
+    float humidity_DHT;
+
+  //Printeo de variables en seriales
   
-	double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-   
-	tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
-  
-   	float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
- 
-  	/*  replaced
-    	float tempVolts = tempReading * 5.0 / 1024.0;
-    	float tempC = (tempVolts - 0.5) * 10.0;
-    	float tempF = tempC * 9.0 / 5.0 + 32.0;
-  	*/	
-				
-  	Serial.println("---TERMISTOR---");		
-  	Serial.println("Temp (C):"); Serial.print( tempC);
-	
-	delay (500);
-				
- //Módulo temperatura y humedad
-  
-  	Serial.println(" ");
-  	Serial.println("---SENSOR TEMP & HUMIDITY---");
-					  
-  	byte temperature = 0;   // (read with raw sample data).
-  	byte humidity = 0;
-  	byte data[40] = {0};
-  	if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
-    		Serial.println("Read DHT11 failed");
-    	return;
-  	}
- 
-  	Serial.println("Temp (C):"); Serial.print( (int)temperature);
-  	Serial.println("Humidity (%):"); Serial.print( (int)humidity);
-  
-  	delay(1000); 	// DHT11 sampling rate is 1HZ.					
+    if( measure_environment( &tempC_DHT, &humidity_DHT ) == true ){
+      Serial.print(tempC_term); 
+      Serial.print(";"); 
+      Serial.print( tempC_DHT, 1 ); 
+      Serial.print(";"); 
+      Serial.print( humidity_DHT, 1 ); 
+      Serial.println(";");
+    }
+
+    if( measure_environment( &tempC_DHT, &humidity_DHT ) == true ){
+      mySerial.print(tempC_term); 
+      mySerial.print(";"); 
+      mySerial.print( tempC_DHT, 1 ); 
+      mySerial.print(";"); 
+      mySerial.print( humidity_DHT, 1 ); 
+      mySerial.println(";");
+    }
+  }					
 }
+
+//Función de confirmación de variables medidas
+
+  static bool measure_environment( float *tempC_DHT, float *humidity_DHT )
+  {
+    static unsigned long measurement_timestamp = millis( );
+  
+    /* Measure once every four seconds. */
+    if( millis( ) - measurement_timestamp > 3000ul )
+    {
+      if( dht_sensor.measure( tempC_DHT, humidity_DHT ) == true )
+      {
+        measurement_timestamp = millis( );
+        return( true );
+      }
+    }
+  
+    return( false );
+  }		
