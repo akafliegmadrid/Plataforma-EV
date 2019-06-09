fd9cac1d545e7ad921a2de3b37fd5c31e21da24c
Andryw4 <andresmateo.mg@gmail.com>
2019-06-09 19:39:15 +0100

    Nuevos botones para comms y termicas

--- sensores.py
+++ sensores.py
@@ -41,7 +41,7 @@ import time                                                # Funciones relaciona
 
 #### PARAMETROS ####
 # Tiempo entre muestras [ms]
-sampleTime = 100
+sampleTime = 200
 
 # Direccion Bluetooth de los Arduino
 btAddr1 = "60:64:05:CF:D3:FC"
@@ -52,11 +52,16 @@ btRead1 = UUID(0xffe1)
 btRead2 = UUID(0xffe1)
 
 # Definicion de pines
-startStopBtn = 2    # Boton que inicia/detiene la lectura BT (Pull-up)
-readingLED   = 3    # LED que indica que se están guardando datos
+startStopBtn = 17   # [GPIO17] Boton que inicia/detiene la lectura BT (Pull-up)
+thermalBtn   = 27   # [GPIO27] Boton que inicia/detine el indicador de termicas
+readingLED   = 22   # [GPIO22] LED que indica que se están guardando datos
+thermalLED   = 23   # [GPIO23] LED que indica que el indicador de termicas esta activado
 
 # Tiempo que hay que mantener pulsado el boton de inicio/parada [ms]
-startStopThres = 2000
+startStopThres = 1000
+
+# Texto que se escribe en los logs cuando se pulsa el boton de termica
+thermalToggleString = "--> Thermal Toggle <--"
 
 #### FUNCIONES ####
 # Funcion handle de las notificaciones
@@ -74,7 +79,7 @@ class BtDelegate(DefaultDelegate):
 
 # Funcion para crear el archivo log a partir de la fecha en la que se ejecuta el programa
 def start_log(num):
-    logfileName = time.strftime("%Y_%m_%d_%H_%M.log", time.gmtime())
+    logfileName = time.strftime("%Y_%m_%d_%H_%M_%S.log", time.gmtime())
     logfileName = "sensor{}_".format(num) + logfileName
     logfile = open(logfileName, 'w')
     logfile.write("######### Plataforma EV - Akaflieg Madrid #########\n")
@@ -85,74 +90,91 @@ def start_log(num):
 # Funcion de escritura de los datos recibidos
 def write2file(dev, fh, start, data):
     data = data.decode('utf-8')
-    fh.write("{:<17.6f} {:s}\n".format(time.time()-start, data))
-    time.sleep(sampleTime/1000)
+    fh.write(data)
 
-# Bucle de ejecucion para cada sensor
-def read_sensor(dev):
-    while True:
-        if GPIO.event_detected(startStopBtn): startTime = time.time()
-        dev.waitForNotifications(startStopThres/1000)   # Se espera al menos 'startStopThres'
-        print("in the loop")
-        if not GPIO.input(startStopBtn) and time.time()-startTime >= startStopThres/1000: break
-    
 #### MAIN ####
 # Interfaces GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(startStopBtn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(thermalBtn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(readingLED, GPIO.OUT)
-GPIO.add_event_detect(startStopBtn, GPIO.FALLING)
+GPIO.setup(thermalLED, GPIO.OUT)
 GPIO.output(readingLED, GPIO.LOW)
+GPIO.output(thermalLED, GPIO.LOW)
 
-while True:
-    # Se empiezan las medidas si el boton esta pulsado 'startStopThres' milisegundos
-    if GPIO.event_detected(startStopBtn):
-        startTime = time.time()
-        GPIO.wait_for_edge(startStopBtn, GPIO.RISING, timeout=startStopThres+1)
-        if (time.time()-startTime) < startStopThres/1000: continue
-    else:
-        continue
-
-    # Se enciende el LED
-    GPIO.output(readingLED, GPIO.HIGH)
-    
-    # Delay de 3 segundos tras el inicio
-    time.sleep(3)
-
-    # Creacion de archivos log
-    start1, log1 = start_log(btAddr1[-2:])
-    start2, log2 = start_log(btAddr2[-2:])
-
-    # Conexion a los sensores
-    btDev1 = Peripheral(btAddr1)
-    btDev2 = Peripheral(btAddr2)
-
-    # Seleccion de las caracteristicas de lectura
-    btCh1 = btDev1.getCharacteristics(uuid=btRead1)[0]
-    btCh2 = btDev2.getCharacteristics(uuid=btRead2)[0]
-
-    # Asignacion del "delegate" (tambien se activan las notificaciones)
-    btDev1.setDelegate(BtDelegate(btDev1,btCh1.valHandle,log1,start1))
-    btDev2.setDelegate(BtDelegate(btDev2,btCh2.valHandle,log2,start2))
-
-    # Lectura de sensores en paralelo
+try:
     while True:
-        if GPIO.event_detected(startStopBtn): startTime = time.time()
-        btDev1.waitForNotifications(startStopThres/2000)   # Se espera al menos 'startStopThres'
-        btDev2.waitForNotifications(startStopThres/2000)   # Se espera al menos 'startStopThres'
-        print("in the loop")
-        if not GPIO.input(startStopBtn) and time.time()-startTime >= startStopThres/1000: break
+        # Se empiezan las medidas si el boton esta pulsado 'startStopThres' milisegundos
+        pressed = False
+        while True:
+            if not GPIO.input(startStopBtn) and not pressed:
+                startTime = time.time()
+                pressed   = True
+            elif GPIO.input(startStopBtn):
+                startTime = time.time()
+                pressed   = False
+            if (time.time()-startTime) >= startStopThres/1000: break
 
-    # Archivos log terminados
-    log1.close()
-    log2.close()
-
-    # Desconexion de los sensores antes de terminar
-    btDev1.disconnect()
-    btDev2.disconnect()
+        # Se enciende el LED
+        GPIO.output(readingLED, GPIO.HIGH)
     
-    # Se apaga el LED
-    GPIO.output(readingLED, GPIO.LOW)
+        # Delay de 3 segundos tras el inicio
+        time.sleep(3)
 
-    # Delay de 3 segundos hasta que se pueda iniciar otra vez
-    time.sleep(3)
+        # Creacion de archivos log
+        start1, log1 = start_log(btAddr1[-2:])
+        start2, log2 = start_log(btAddr2[-2:])
+
+        # Conexion a los sensores
+        btDev1 = Peripheral(btAddr1)
+        btDev2 = Peripheral(btAddr2)
+
+        # Seleccion de las caracteristicas de lectura
+        btCh1 = btDev1.getCharacteristics(uuid=btRead1)[0]
+        btCh2 = btDev2.getCharacteristics(uuid=btRead2)[0]
+
+        # Asignacion del "delegate" (tambien se activan las notificaciones)
+        btDev1.setDelegate(BtDelegate(btDev1,btCh1.valHandle,log1,start1))
+        btDev2.setDelegate(BtDelegate(btDev2,btCh2.valHandle,log2,start2))
+
+        # Lectura de sensores en serie
+        StartStopPressed = False
+        thermalPressed   = False
+        thermalLEDState  = False
+        while True:
+            # Se sale del modo de medida si se pulsa el boton
+            if not GPIO.input(startStopBtn) and not pressed:
+                startTime = time.time()
+                StartStopPressed = True
+            elif GPIO.input(startStopBtn):
+                startTime = time.time()
+                StartStopPressed = False
+            # Se cambia el modo termica si se pulsa el boton
+            if not GPIO.input(thermalBtn) and not thermalPressed:
+                log1.write(thermalToggleString)
+                log2.write(thermalToggleString)
+                thermalPressed = True
+                thermalLEDState = not thermalLEDState
+                GPIO.output(thermalLED, thermalLEDState)
+            elif GPIO.input(thermalBtn):
+                thermalPressed = False
+            while btDev1.waitForNotifications(sampleTime/1000): pass
+            while btDev2.waitForNotifications(sampleTime/1000): pass
+            if not GPIO.input(startStopBtn) and time.time()-startTime >= startStopThres/1000: break
+
+        # Archivos log terminados
+        log1.close()
+        log2.close()
+
+        # Desconexion de los sensores antes de terminar
+        btDev1.disconnect()
+        btDev2.disconnect()
+    
+        # Se apaga el LED
+        GPIO.output(readingLED, GPIO.LOW)
+
+        # Delay de 3 segundos hasta que se pueda iniciar otra vez
+        time.sleep(3)
+        
+except KeyboardInterrupt:
+    GPIO.cleanup()
