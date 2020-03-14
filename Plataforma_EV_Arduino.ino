1c4656f07f1ed6f9064327e4637745eaab511ffe
Andryw4 <andresmateo.mg@gmail.com>
2020-03-14 21:30:06 +0100

    Codigo Arduino para Nano y Nano Every

--- Plataforma_EV_Arduino.ino
+++ Plataforma_EV_Arduino.ino
@@ -4,8 +4,14 @@
 // *** UNIVERSIDAD POLITECNICA DE MADRID ***
 // *****************************************
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+/* Parametros */
+const uint32_t sampleTime = 500;  // [ms]
+
+/* Variables locales */
 typedef struct {
   uint32_t timestamp;
   float temperature;
@@ -13,21 +19,31 @@ typedef struct {
   float humidity;
 } data_t;
 
-const uint32_t sampleTime = 500;  // [ms]
 uint32_t readingTime;
 data_t data;
 Adafruit_BME280 sensor;
 
+/* Setup */
 void setup () {
 
+#ifdef ARDUINO_AVR_NANO_EVERY
+  Serial1.begin(9600);
+#elif ARDUINO_AVR_NANO
   Serial.begin(9600);
-  if (!sensor.begin()) {
+#endif
+
+  if (!sensor.begin(0x76)) {
+#ifdef ARDUINO_AVR_NANO_EVERY
+    Serial1.println("----- Sensor not found -----");
+#elif ARDUINO_AVR_NANO
     Serial.println("----- Sensor not found -----");
+#endif
     while(true);
   }
 
 }
 
+/* Bucle principal */
 void loop () {
 
   readingTime = millis();
@@ -39,6 +55,7 @@ void loop () {
 
 }
 
+/* Funcion de lectura */
 void read_data (data_t &data) {
 
   data.timestamp   = millis();
@@ -48,11 +65,19 @@ void read_data (data_t &data) {
 
 }
 
+/* Funcion de escritura */
 void send_data (data_t data) {
 
+#ifdef ARDUINO_AVR_NANO_EVERY
+  Serial1.print(data.timestamp);   Serial1.print(',');
+  Serial1.print(data.temperature); Serial1.print(',');
+  Serial1.print(data.pressure);  Serial1.print(',');
+  Serial1.println(data.humidity);
+#elif ARDUINO_AVR_NANO
   Serial.print(data.timestamp);   Serial.print(',');
   Serial.print(data.temperature); Serial.print(',');
-  Serial.print(data.pressure);    Serial.print(',');
+  Serial.print(data.pressure);  Serial.print(',');
   Serial.println(data.humidity);
+#endif
 
 }
